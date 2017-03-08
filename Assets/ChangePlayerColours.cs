d4dabe4791822bbda6b0eadf7dddff3ce55d2ea3
Nick Stacey <nick@ditt.to>
2017-03-08 08:31:51 +0000

    Initial import

---
+++ Assets/ChangePlayerColours.cs
@@ -0,0 +1,29 @@
+﻿// ChangePlayerColours.cs
+
+using Player.SyncedData;
+using Player.Tracking;
+using UnityEngine;
+
+class ChangePlayerColours:MonoBehaviour {
+
+    public void Start ()
+    {
+        foreach (GameObject player in PlayerTracker.GetInstance().GetPlayers()) {
+            AddColourChangeEvent(player);
+        }
+        PlayerTracker.GetInstance().OnPlayerAdded += AddColourChangeEvent;
+    }
+
+    public void AddColourChangeEvent (GameObject player)
+    {
+        PlayerDataForClients playerData = player.GetComponent<PlayerDataForClients>();
+        HandlePlayerColourChange(player, playerData.GetTeam());
+        playerData.OnTeamUpdated += HandlePlayerColourChange;
+    }
+
+    public void HandlePlayerColourChange (GameObject player, int newTeam)
+    {
+        player.GetComponentInChildren<MeshRenderer>().material.color = newTeam == PlayerDataForClients.TEAM_VIP ? Color.red : Color.blue;
+    }
+}
+
